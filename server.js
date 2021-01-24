e8f312589a5bac316cda7b9216f5fbd3833433d3
Lisa Herzberg <lisamarieherzberg@gmail.com>
2021-01-23 20:09:24 -0700

    added chat ui package

--- server.js
+++ server.js
@@ -3,13 +3,36 @@
 const PORT = 3000
 const io = require('socket.io')();
 
+let userIds = {};
+let currentUserId = 2;
+let currentMessageId = 1;
+
+function createMessage(userId, messageText){
+    return {
+        _id: currentMessageId++,
+        text: messageText,
+        createdAt: new Date(),
+        user: {
+            id: userId,
+            name: 'test',
+            avatar: 'https://placeimg.com/140/140/any',
+        }
+    }
+}
 
 io.sockets.on('connection', (socket) => {
-    console.log('a user connected')
+    console.log('a user connected');
 
-    socket.on('message', (message) => {
-        io.emit('message', message)
-    })
+    userIds[socket.id] = currentUserId++;
+
+    socket.on('message', (messageText) => {
+        const userId = userIds[socket.id];
+        const message = createMessage(userId, messageText);
+        socket.broadcast.emit('message', message)
+    });
+
+
+
 
     socket.on('turn', (turnObj) => {
         console.log(turnObj)
--- tictactoern/src/Screens/ChatScreen.js
+++ tictactoern/src/Screens/ChatScreen.js
@@ -1,46 +1,37 @@
 import React, {useEffect, useState, useRef} from 'react';
-import {Button, StyleSheet, Text, TextInput, View, TouchableOpacity} from "react-native";
+import { StyleSheet,  View} from "react-native";
 import io from "socket.io-client/build/index";
+import { GiftedChat } from 'react-native-gifted-chat';
+
 
 
 function ChatScreen({ route, navigation }) {
-    const [newMessage, setNewMessage] = useState("");
-    const [messageArr, setMessageArr] = useState([])
+    const [messageArr, setMessageArr] = useState([]);
     const socket = useRef(null);
 
     useEffect(() => {
         socket.current = io("http://127.0.0.1:3000");
         socket.current.on('message', (message) => {
-            setMessageArr(prevState => [...prevState, message])
+            setMessageArr(previousMessages => GiftedChat.append(previousMessages, message))
         })
     },[]);
 
-    const handleSendMessage = () => {
-        socket.current.emit('message', newMessage)
-        setNewMessage("");
-    };
-
-    const renderMessages = () => {
-        return messageArr.map((e,i) => {
-            return (
-                <View style={styles.message}>
-                    <Text key={`${e}-${i}`}>{e}</Text>
-                </View>
-            )
-        })
+    const onSend = (messages) => {
+        socket.current.emit('message', messages[0].text)
+        setMessageArr(previousMessages => GiftedChat.append(previousMessages, messages))
     }
+
+
+
     return (
         <View style={styles.container}>
-            {renderMessages()}
-            <View style={styles.chatInputWrapper}>
-                <TextInput
-                    style={styles.input}
-                    value={newMessage}
-                    onChangeText={(value) => setNewMessage(value)}/>
-                <TouchableOpacity style={styles.buttonWrapper} onPress={handleSendMessage}>
-                    <Text style={styles.button}>Send</Text>
-                </TouchableOpacity>
-            </View>
+            <GiftedChat
+                messages={messageArr}
+                onSend={messages => onSend(messages)}
+                user={{
+                    _id: 1,
+                }}
+            />
         </View>
     );
 }
@@ -48,31 +39,8 @@ function ChatScreen({ route, navigation }) {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'flex-end',
-        marginHorizontal: 15
+        backgroundColor: 'white'
     },
-    chatInputWrapper: {
-        flexDirection: 'row',
-        paddingBottom: 50,
-    },
-    message: {
-        padding: 10
-    },
-    input: {
-        flex: 1,
-        height: 40,
-        borderWidth: 1
-    },
-    buttonWrapper: {
-        backgroundColor: 'blue',
-        justifyContent: 'center',
-        alignItems: 'center',
-        padding: 5
-    },
-    button: {
-        color: 'white',
-        fontSize: 18
-    }
 });
 
 export default ChatScreen
